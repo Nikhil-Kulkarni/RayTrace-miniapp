ee1a010a6401149403f4625d5d0315923ce7dc63
Nikhil Kulkarni <nikhil896@gmail.com>
2017-09-20 21:48:43 -0400

    Changes to configure

---
+++ build/include/CompilerFeatures.h
@@ -0,0 +1,8 @@
+// This is a automatically generated file to set define variables for supported C++ features
+#define  ENABLE_SHARED_PTR
+#define  ENABLE_STD_FUNCTION
+#define  ENABLE_STD_TUPLE
+#define  ENABLE_VARIADIC_TEMPLATE
+#define  ENABLE_THREAD_LOCAL
+#define  ENABLE_MOVE_CONSTRUCTOR
+#define  ENABLE_STATIC_ASSERT
